915cab1ae527404bbf018e3423c3670c234b86f9
Ravin Kumar <16964978+mr-ravin@users.noreply.github.com>
2022-08-23 18:01:41 +0530

    Update bingenetic.py

--- bingenetic.py
+++ bingenetic.py
@@ -1,41 +1,47 @@
 def gtb(gen_code_list,mode="dna"): # genetic code to binary code
   res_list=[]
+  mode = mode.lower()
   if mode=="dna":
     for gen_code_line in gen_code_list:
        tmp=""
        elem_idx=0
        gen_code_line_len=len(gen_code_line)
        while elem_idx<gen_code_line_len:
-           if gen_code_line[elem_idx]=="A":
+           if gen_code_line[elem_idx].upper()=="A":
              tmp=tmp+"00"
-           elif gen_code_line[elem_idx]=="T":
+           elif gen_code_line[elem_idx].upper()=="T":
+             tmp=tmp+"11"
+           elif gen_code_line[elem_idx].upper()=="C":
              tmp=tmp+"01"
-           elif gen_code_line[elem_idx]=="C":
+           elif gen_code_line[elem_idx].upper()=="G":
              tmp=tmp+"10"
            else:
-             tmp=tmp+"11"
+             raise TypeError("Not a valid DNA Sequence")
            elem_idx=elem_idx+1
        res_list.append(tmp)
-  else:
+  elif mode=="rna":
     for gen_code_line in gen_code_list:
        tmp=""
        elem_idx=0
        gen_code_line_len=len(gen_code_line)
        while elem_idx<gen_code_line_len:
-           if gen_code_line[elem_idx]=="A":
+           if gen_code_line[elem_idx].upper()=="A":
              tmp=tmp+"00"
-           elif gen_code_line[elem_idx]=="U":
+           elif gen_code_line[elem_idx].upper()=="U":
+             tmp=tmp+"11"
+           elif gen_code_line[elem_idx].upper()=="C":
              tmp=tmp+"01"
-           elif gen_code_line[elem_idx]=="C":
+           elif gen_code_line[elem_idx].upper()=="G":
              tmp=tmp+"10"
            else:
-             tmp=tmp+"11"
+             raise TypeError("Not a valid RNA Sequence")
            elem_idx=elem_idx+1
        res_list.append(tmp)
   return res_list
 
 def btg(bin_code_list,mode="dna",EOF_CODE=""): # binary code to genetic code, here EOF_CODE is a special binary code to make binary numbers even
   res_list=[]
+  mode = mode.lower()
   if mode=="dna":
     for bin_code_line in bin_code_list:
       tmp=""
@@ -48,14 +54,16 @@ def btg(bin_code_list,mode="dna",EOF_CODE=""): # binary code to genetic code, he
         if bin_code_line[elem_idx]=="0" and bin_code_line[elem_idx+1]=="0":
           tmp=tmp+"A"
         elif bin_code_line[elem_idx]=="0" and bin_code_line[elem_idx+1]=="1":
-          tmp=tmp+"T"
-        elif bin_code_line[elem_idx]=="1" and bin_code_line[elem_idx+1]=="0":
           tmp=tmp+"C"
-        else:
+        elif bin_code_line[elem_idx]=="1" and bin_code_line[elem_idx+1]=="0":
           tmp=tmp+"G"
+        elif bin_code_line[elem_idx]=="1" and bin_code_line[elem_idx+1]=="1":
+          tmp=tmp+"T"
+        else:
+          raise TypeError("Not a valid binary sequence of the DNA")
         elem_idx=elem_idx+2
       res_list.append(tmp)
-  else:
+  elif mode=="rna":
     for bin_code_line in bin_code_list:
       tmp=""
       elem_idx=0
@@ -67,11 +75,13 @@ def btg(bin_code_list,mode="dna",EOF_CODE=""): # binary code to genetic code, he
         if bin_code_line[elem_idx]=="0" and bin_code_line[elem_idx+1]=="0":
           tmp=tmp+"A"
         elif bin_code_line[elem_idx]=="0" and bin_code_line[elem_idx+1]=="1":
-          tmp=tmp+"U"
-        elif bin_code_line[elem_idx]=="1" and bin_code_line[elem_idx+1]=="0":
           tmp=tmp+"C"
-        else:
+        elif bin_code_line[elem_idx]=="1" and bin_code_line[elem_idx+1]=="0":
           tmp=tmp+"G"
+        elif bin_code_line[elem_idx]=="1" and bin_code_line[elem_idx+1]=="1":
+          tmp=tmp+"U"
+        else:
+          raise TypeError("Not a valid binary sequence of the RNA")
         elem_idx=elem_idx+2
       res_list.append(tmp)
   return res_list
